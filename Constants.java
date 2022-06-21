368aa8f8e3a278c52506b0a56dd3ced81fad0263
RedwanaFarbin <78018600+redwanafarbi@users.noreply.github.com>
2022-06-22 00:00:50 +0600

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,20 @@
+public abstract class Constants {
+    public static String FILE_NAME = "students.txt";
+    public static String DATA_LOADING = "Loading data ...";
+    public static String DATA_LOADED = "Data Loaded.";
+    public static String CHAR_A = "a";
+    public static String CHAR_C = "c";
+    public static String CHAR_R = "r";
+    public static String QUESTION_MARK = "?";
+    public static String PLUS_SIGN = "+";
+    public static String COMMA_SIGN = ",";
+    public static String COMMA_AND_SPACE_SIGN = "[ ,]+";
+    public static String ERROR_MESSAGE_READING = "Error reading file";
+    public static String ERROR_MESSAGE_WRITING = "Error writing file";
+    public static String DATE_FORMAT_MODEL = "dd-mm-yyyy hh:mm:ss a";
+    public static String FOUND = "We found it!";
+    public static String WORDS_FOUND = " word(s) found";
+    public static String LAST_UPDATE = "\nList last updated on ";
+    public static String ERROR_MESSAGE_WRONG_ARGUMENTS = "You have not provide proper argument. Please provide a|c|r|+{New_Students}|{Students_Name}";
+
+}
